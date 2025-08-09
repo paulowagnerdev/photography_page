bc792724605d646f368ce3a053cf5d5adc1795a3
paulowagnerdev <paulo.wagner@live.com>
2025-08-09 11:34:45 -0300

    Last Update 09/08

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 const observer = new IntersectionObserver((entries,observer) => {
     entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.target;
-        }
-    })
-})
 
-const imgs = document.querySelectorAll(".img");
+        if(!entry.isIntersecting) return
+
+        const imagem = entry.target;
+        imagem.src = imagem.src.replace("&w=10&", "&w=2000&");
+        console.log(imagem.src);
+    })
+}, {})
+
+const imgs = document.querySelectorAll(".lazy");
 
 imgs.forEach(img => {
     observer.observe(img);
 })
 
+
+window.scrollTo({
+    top: '0',
+    behavior: 'smooth'
+})
